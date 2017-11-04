52163c840469f3f17168a5e5a8860ff143b7c375
krishnaUIDev <krishna.kondoju007@gmail.com>
2017-11-03 22:22:07 -0500

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GroceryItemComponent } from './grocery-item/grocery-item.component';
+import { GroceryListComponent } from './grocery-list/grocery-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryItemComponent,
+    GroceryListComponent
   ],
   imports: [
     BrowserModule
